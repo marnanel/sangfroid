f9bcce80f1d90124ab40dec077ed2a4148379b43
Marnanel Thurman <marnanel@thurman.org.uk>
2023-12-13 20:47:42 +0000

    circles.sif has gamma and res different for different dimensions
    So that we can pick up if they're reversed

--- test/test_load.py
+++ test/test_load.py
@@ -7,8 +7,8 @@ def test_load_sif():
     assert sif.name == 'circles'
     assert sif.description == 'I like circles. They are round.'
     assert sif.size == (480, 270)
-    assert sif.resolution==(2834.645669, 2834.645669)
-    assert sif.gamma==(1.0, 1.0, 1.0)
+    assert sif.resolution==(2834.645669, 2835)
+    assert sif.gamma==(0.98, 0.99, 1.0)
 
 def test_load_sifz():
     sif = get_animation('wombats.sifz')
