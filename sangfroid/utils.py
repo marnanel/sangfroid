a6b2ad9fce47934dbc634e87ec6365b2b39bc32b
Marnanel <marnanel@thurman.org.uk>
2023-12-29 21:01:27 +0000

    implement the rest of Boolean
    test added

--- sangfroid/utils.py
+++ sangfroid/utils.py
@@ -1,14 +1,34 @@
 class Boolean:
-    def __init__(self, value):
-        if value=='true' or value==1:
+    def __init__(self, value=0):
+        if isinstance(value, Boolean):
+            value = value.value
+        elif isinstance(value, str):
+            value = value.lower()
+
+        if value=='true':
             self.value = True
-        elif value=='false' or value==0:
+        elif value=='false':
             self.value = False
+        elif isinstance(value, bool):
+            self.value = value
+        elif isinstance(value, int):
+            self.value = value!=0
         else:
             raise ValueError("{value} is not a valid description of a boolean")
 
+        assert isinstance(self.value, bool)
+
     def __bool__(self):
         return self.value
+
+    def __int__(self):
+        if self.value:
+            return 1
+        else:
+            return 0
+
+    def __eq__(self, other):
+        return self.value==bool(other)
 
     def __str__(self):
         if self.value:
--- test/test_utils.py
+++ test/test_utils.py
@@ -13,3 +13,30 @@ def test_utils_fps():
     wombat = bs4.Tag(name='wombat')
     with pytest.raises(ValueError):
         tag_to_fps(wombat)
+
+def test_utils_boolean():
+
+    for thing, expected in [
+            ("Boolean()",           False),
+            ("Boolean(False)",      False),
+            ("Boolean(0)",          False),
+            ("Boolean(Boolean(0))", False),
+            ("Boolean(True)",       True),
+            ("Boolean(1)",          True),
+            ("Boolean(Boolean(1))", True),
+            ]:
+
+        b = eval(thing)
+
+        if expected:
+            assert b, thing
+            assert b==True, thing
+            assert bool(b)==True, thing
+            assert int(b)==1, thing
+            assert str(b)=='true', thing
+        else:
+            assert not b, thing
+            assert b==False, thing
+            assert bool(b)==False, thing
+            assert int(b)==0, thing
+            assert str(b)=='false', thing
