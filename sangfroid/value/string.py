27625d45c7506b19fc849e3281894934e5af8a2d
Marnanel <marnanel@thurman.org.uk>
2024-01-22 22:51:58 +0000

    String values are read and written correctly
    They were being treated as attributes, like floats. They're actually just stored within the tag.
    
    Regression test added.

---
+++ sangfroid/value/string.py
@@ -0,0 +1,17 @@
+import bs4
+from sangfroid.value.value import Value
+from sangfroid.t import T
+from sangfroid.utils import tag_to_fps
+
+@Value.handles_type()
+class String(Value):
+    our_type = str
+
+    @property
+    def value(self):
+        result = str(self.tag.string)
+        return result
+
+    @value.setter
+    def value(self, v):
+        self.tag.string = str(v)
