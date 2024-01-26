c2413a40da3318525950abb73acc6c6c23a4b916
Marnanel <marnanel@thurman.org.uk>
2024-01-26 22:23:45 +0000

    Added docstrings to Animation
    Animation.begin_time and end_time are in T, not float

--- sangfroid/animation.py
+++ sangfroid/animation.py
@@ -3,9 +3,27 @@ from sangfroid.keyframe import Keyframe
 from sangfroid.layer import Group
 from sangfroid.format import Format
 from sangfroid.value.color import Color
+from sangfroid.t import T
 
 class Animation(Group):
-    def __init__(self, filename):
+    """
+    A Synfig animation.
+
+    Synfig calls this a "canvas", but it also has a layer attribute
+    called a "canvas". At first we called it "Sif", but that was
+    no good, because it might be loaded from a `.sifz` or `.sfg` file.
+
+    At present there's no way to create a new, blank canvas.
+    You have to create one in Synfig Studio first.
+    This will be fixed soon.
+
+    Most of the properties don't yet have setters. They will.
+    """
+    def __init__(self, filename:str):
+        """
+        Args:
+            filename: the name of the main file to load.
+        """
         self.filename = filename
         self.format = Format.from_filename(filename)
 
@@ -23,14 +41,33 @@ class Animation(Group):
 
     @property
     def name(self):
+        """
+        The name of this animation.
+
+        Not the filename, though it's often the same.
+        """
         return self.tag.find('name').string
 
     @property
     def description(self):
+        """
+        A description of this animation.
+
+        So you know what it is when you find it again next year.
+
+        Type:
+            str
+        """
         return self.tag.find('desc').string
 
     @property
     def size(self):
+        """
+        The height and width of the screen.
+
+        Type:
+            (int, int)
+        """
         return (
                 int(self.tag.attrs['width']),
                 int(self.tag.attrs['height']),
@@ -38,45 +75,82 @@ class Animation(Group):
 
     @property
     def resolution(self):
+        """
+        The default resolution.
+
+        Type:
+            (float, float)
+        """
         return (
                 float(self.tag.attrs['xres']),
                 float(self.tag.attrs['yres']),
                 )
 
     @property
-    def gamma(self):
-        return (
-                float(self.tag.attrs['gamma-r']),
-                float(self.tag.attrs['gamma-g']),
-                float(self.tag.attrs['gamma-b']),
-                )
-
-    @property
     def viewbox(self):
+        """
+        The coordinates of the edges of the screen.
+
+        This is a tuple of four floats:
+        `(left, bottom, right, top)`.
+
+        (Maybe it would be sensible to have e.g.
+        `Animation.top` etc?)
+
+        Type:
+            4-tuple of floats
+        """
         return tuple([
             float(n) for n in
             self.tag.attrs['view-box'].split(' ')
             ])
 
     @property
-    def antialias(self):
-        # XXX what is the type?
-        return float(self.tag.attrs['antialias'])
- 
-    @property
     def fps(self):
+        """
+        The number of frames per second. Usually 24.
+
+        (Can this be non-integer?)
+
+        Type:
+            float
+        """
         return float(self.tag.attrs['fps'])
  
     @property
     def begin_time(self):
-        return self.time_to_frames(self.tag.attrs['begin-time'])
+        """
+        The time at which this animation starts.
+
+        Almost always zero.
+
+        Type:
+            T
+        """
+        return T(self.tag.attrs['begin-time'],
+                 fps=self.fps,
+                 )
 
     @property
     def end_time(self):
-        return self.time_to_frames(self.tag.attrs['end-time'])
+        """
+        The time at which this animation ends.
+
+        Type:
+            T
+        """
+        return T(self.tag.attrs['end-time'],
+                 fps=self.fps,
+                 )
 
     @property
     def background(self):
+        """
+        The background colour.
+
+        Type:
+            Color
+        """
         triplet = tuple([
             float(n) for n in
             self.tag.attrs['bgcolor'].split(' ')
@@ -86,25 +160,24 @@ class Animation(Group):
 
     @property
     def keyframes(self):
+        """
+        The defined keyframes.
+
+        This is subject to change. At present it's just a generator
+        over the keyframes. But the keyframe collection needs to be
+        a class in itself, so you can add and delete them.
+        """
         for kf in self.sif.tag.find_all('keyframe'):
             yield Keyframe.from_tag(kf)
  
-    @property
-    def contents(self):
-        yield 'no'
+    def save(self, filename:str=None):
+        """
+        Saves the animation back out to disk.
 
-    def time_to_frames(self, t):
-        if ' ' in t:
-            return sum([self.time_to_frames(n) for n in t.split(' ')])
-
-        if t.endswith('s'):
-            return int(float(t[:-1])*self.fps)
-        elif t.endswith('f'):
-            return int(t[:-1])
-        else:
-            raise ValueError(f"I don't understand the time specification: {t}")
-
-    def save(self, filename=None):
+        Args:
+            filename: the filename to save the animation to.
+                If None, we use the filename we loaded it from.
+        """
         self.format.save(
                 content = self.soup,
                 filename = filename,
--- test/test_animation.py
+++ test/test_animation.py
@@ -10,8 +10,9 @@ def test_animation_load_sif():
     assert sif.description == 'I like circles. They are round.'
     assert sif.size == (480, 270)
     assert sif.resolution==(2834.645669, 2835)
-    assert sif.gamma==(0.98, 0.99, 1.0)
     assert sif.background==sangfroid.value.Color('#808080')
+    assert sif.begin_time==0
+    assert sif.end_time==sangfroid.T('5s', fps=sif.fps)
 
 def test_animation_load_sifz():
     sif = get_animation('wombats.sifz')
