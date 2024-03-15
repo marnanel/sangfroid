eff6e9258a1f2c63b3a67c298d4f266de7dfef8c
Marnanel <marnanel@thurman.org.uk>
2024-03-15 02:01:45 +0000

    + Import

--- sangfroid/layer/__init__.py
+++ sangfroid/layer/__init__.py
@@ -1,7 +1,9 @@
 from sangfroid.layer.layer import *
+
 from sangfroid.layer.blurs import *
 from sangfroid.layer.geometry import *
 from sangfroid.layer.group import *
+from sangfroid.layer.other import *
 from sangfroid.layer.stylise import *
 from sangfroid.layer.time import *
 from sangfroid.layer.transform import *
---
+++ sangfroid/layer/other.py
@@ -0,0 +1,13 @@
+from sangfroid.layer import Layer
+import sangfroid.value as v
+
+@Layer.handles_type()
+class Import(Layer):
+    SYMBOL = 'I'
+    PARAMS = {
+        "z_depth": v.Real,
+        "amount": v.Real,
+        "blend_method": v.Integer,
+        "size": v.Vector,
+        "transformation": v.Composite,
+    }
