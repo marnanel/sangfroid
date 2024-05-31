2b0720b57c0eedeeba19b517974a470683b525c4
Marnanel <marnanel@thurman.org.uk>
2024-05-31 20:58:09 +0100

    test which fails

--- test/value/test_transformation.py
+++ test/value/test_transformation.py
@@ -13,3 +13,13 @@ def test_transformation_simple():
             'skew_angle': 50.5,
             'scale': (2.0, 0.5),
             }
+
+def test_transformation_shortcuts():
+    sif = get_animation('circles.sif')
+
+    circle = sif.find(desc="Well, it's round")
+
+    assert circle.offset == (3.3333332539, -0.8333333135)
+    assert circle.angle == 45.0
+    assert circle.skew_angle == 50.5
+    assert circle.scale == (2.0, 0.5)
